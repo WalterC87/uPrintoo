4d3cc4992cecaea9de4d56e796a72e4a117d5176
Walter Cordero <wdlara.8789@gmail.com>
2014-06-03 17:26:28 -0600

    last upgrade

--- public/js/printoo.js
+++ public/js/printoo.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(".arrow-down").click(function(event){        
+	$(".arrow-down, .top-link").click(function(event){        
         event.preventDefault();
     	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 5000);
     });
@@ -17,9 +17,9 @@ $(document).ready(function(){
 	 $('#div-6').parallax("50%", 0.1);   
 	 $('#div-7').parallax("50%", 0.1);   
 
-    $('.submit').on('click',function(){
+    /*$('.submit').on('click',function(){
         document.forms['newlester_register_form'].submit();
-    });
+    });*/
 
     if( $(".animated")[0] ) {
         $('.animated').css('opacity','0');
